654b4bc1db5849c40433bb7311b86703cbf141c3
Kyrylo <kbuchinskiy1@gmail.com>
2024-04-22 14:53:07 +0300

    add permission class to booklist

--- books/views.py
+++ books/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 from books.models import Book
 from books.serializers import BookSerializer
@@ -8,3 +8,4 @@ from books.serializers import BookSerializer
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
