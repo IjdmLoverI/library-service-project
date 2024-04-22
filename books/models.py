69757fc4809eba732b674c5a83c0edb730edc27d
Kyrylo <kbuchinskiy1@gmail.com>
2024-04-22 13:19:31 +0300

    add book models

--- books/models.py
+++ books/models.py
@@ -1,1 +1,14 @@
 from django.db import models
+from django.forms import ChoiceField
+
+
+class Book(models.Model):
+    COVER_CHOICES = (
+        ("HARD", "Hard cover"),
+        ("SOFT", "Soft cover")
+    )
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    Cover = ChoiceField(choices=COVER_CHOICES)
+    inventory = models.IntegerField(default=0)
+    daily_fee = models.DecimalField(max_digits=10, decimal_places=2)
