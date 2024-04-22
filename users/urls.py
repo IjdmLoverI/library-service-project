7b4d1d2714f65c691193a6ece9341a183d3dad25
Kyrylo <kbuchinskiy1@gmail.com>
2024-04-22 15:17:25 +0300

    fix permissions.py

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
 from users.views import CreateUserView, ManageUserView
 
@@ -8,6 +8,8 @@ urlpatterns = [
     path("me/", ManageUserView.as_view(), name="manage"),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
+
 ]
 
 app_name = "users"
