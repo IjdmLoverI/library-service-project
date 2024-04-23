68d9659a777c640ce05019c1d334f4a8a0db8a76
Kyrylo <kbuchinskiy1@gmail.com>
2024-04-23 12:36:28 +0300

    fix migrations, add borrowing list nd detail endpoints

--- borrowings/views.py
+++ borrowings/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import mixins, generics
+from rest_framework.permissions import AllowAny
 
-# Create your views here.
+from borrowings.models import Borrowing
+from borrowings.serializers import BorrowingDetailSerializer
+
+
+class BorrowingListView(generics.ListAPIView):
+    queryset = Borrowing.objects.all()
+    serializer_class = BorrowingDetailSerializer
+
+
+class BorrowingDetailView(generics.RetrieveAPIView):
+    queryset = Borrowing.objects.all()
+    serializer_class = BorrowingDetailSerializer
