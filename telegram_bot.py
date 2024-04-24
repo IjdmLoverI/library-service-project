29f857bc4ecdcd7c8f4ed00436e6b95eedd84097
Kyrylo <kbuchinskiy1@gmail.com>
2024-04-24 12:32:38 +0300

    add base notification logic

--- telegram_bot.py
+++ telegram_bot.py
@@ -3,20 +3,20 @@ import django
 import telebot
 import sqlite3
 
-# Set up Django
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'library_service_project.settings')
 django.setup()
 
-# Import Django models after setup
 from users.models import User
+from borrowings.models import Borrowing
+
 
 TELEGRAM_BOT_TOKEN: str = "7043047188:AAHm7qvRAeDwI9brIuOzjn9EW-KKXEzbkfQ"
 bot = telebot.TeleBot(TELEGRAM_BOT_TOKEN)
 
-conn = sqlite3.connect("db.sqlite3.users")
+conn = sqlite3.connect("db.sqlite3")
 cursor = conn.cursor()
 
-# Dictionary to store email temporarily
 user_email = {}
 
 
@@ -38,10 +38,27 @@ def handle_message(message):
             user = User.objects.get(email=email)
             if user.check_password(password):
                 bot.reply_to(message, "Login successful.")
+                check_borrowings(message, user)
             else:
                 bot.reply_to(message, "Invalid email or password.")
         except User.DoesNotExist:
             bot.reply_to(message, "User does not exist.")
+
+
+@bot.message_handler(commands=['borrowings'])
+def check_borrowings(message, user):
+    borrowings = Borrowing.objects.filter(borrower_id=user.id)
+    if borrowings:
+        response = "Your borrowings:\n"
+        for borrowing in borrowings:
+            response += f"- Book: {borrowing.book}\n"
+            response += f"- Borrow date: {borrowing.borrow_date}\n"
+            response += f"- Expiration date: {borrowing.expected_return_date}\n"
+            response += "###############################\n"
+        bot.reply_to(message, response)
+    else:
+        bot.reply_to(message, "You don't have any borrowings.")
+
 
 
 bot.polling()
