60a6dd9d6d60ef30ec586a1ea3f68cd6b357ecaf
Kyrylo <kbuchinskiy1@gmail.com>
2024-04-24 11:32:16 +0300

    setup a base tg bot

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,19 +1,47 @@
+import os
+import django
 import telebot
+import sqlite3
+
+# Set up Django
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'library_service_project.settings')
+django.setup()
+
+# Import Django models after setup
+from users.models import User
 
 TELEGRAM_BOT_TOKEN: str = "7043047188:AAHm7qvRAeDwI9brIuOzjn9EW-KKXEzbkfQ"
-TELEGRAM_BOT_USERNAME: str = "@LiraryBot"
-
 bot = telebot.TeleBot(TELEGRAM_BOT_TOKEN)
 
+conn = sqlite3.connect("db.sqlite3.users")
+cursor = conn.cursor()
 
-@bot.message_handler(commands=["start", "hello"])
-def send_welcome(message):
-    bot.reply_to(message, "Howdy, how are you doing?")
-
-
-@bot.message_handler(func=lambda msg: True)
-def echo_all(message):
-    bot.reply_to(message, message.text)
+# Dictionary to store email temporarily
+user_email = {}
 
 
-bot.infinity_polling()
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(message, "Welcome! Please send your email address.")
+
+
+@bot.message_handler(func=lambda message: True)
+def handle_message(message):
+    chat_id = message.chat.id
+    if chat_id not in user_email:
+        user_email[chat_id] = message.text
+        bot.reply_to(message, "Email received. Please send your password.")
+    else:
+        email = user_email.pop(chat_id)
+        password = message.text
+        try:
+            user = User.objects.get(email=email)
+            if user.check_password(password):
+                bot.reply_to(message, "Login successful.")
+            else:
+                bot.reply_to(message, "Invalid email or password.")
+        except User.DoesNotExist:
+            bot.reply_to(message, "User does not exist.")
+
+
+bot.polling()
